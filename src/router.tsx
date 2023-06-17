b8797df0430a8b9392f82b9a2b92ab012d8768f3
Eduardo <eduardoestentotecinfo@gmail.com>
2023-06-17 18:06:12 -0300

    ci: change content to test CI

--- src/router.tsx
+++ src/router.tsx
@@ -4,9 +4,9 @@ const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/"  element={<h1>Home</h1>} />
-        <Route path="/checkout" element={<h1>Checkout</h1>} />
-        <Route path="/checkout/orderplaced" element={<h1>Order Placed</h1>} />
+        <Route path="/"  element={<h1>Home!</h1>} />
+        <Route path="/checkout" element={<h1>Checkout!</h1>} />
+        <Route path="/checkout/orderplaced" element={<h1>Order Placed!</h1>} />
         <Route path='*' element={<Navigate to='/' />} />
       </Routes>
     </BrowserRouter>
