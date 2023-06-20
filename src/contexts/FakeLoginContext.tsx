099d05bae491ee72752fab3d9697562b60fc2f07
Eduardo <eduardoestentotecinfo@gmail.com>
2023-06-20 18:43:07 -0300

    Create fake login context, wip feature

---
+++ src/contexts/FakeLoginContext.tsx
@@ -0,0 +1,39 @@
+import React, { createContext, useState } from "react";
+
+interface Props {
+  children: React.ReactNode;
+}
+
+type UserInfo = {
+  email: string;
+  setEmail: (value: string) => void;
+  isLogged: boolean,
+  setIsLogged: (value: boolean) => void;
+};
+
+const FakeLoginContext = createContext<UserInfo>({
+  email: '',
+  setEmail: () => {},
+  isLogged: false,
+  setIsLogged: () => {},
+});
+
+export const FakeLoginProvider: React.FC<Props> = ({ children }) => {
+  const [email, setEmail] = useState('');
+  const [isLogged, setIsLogged] = useState(false);
+
+  return (
+    <FakeLoginContext.Provider
+      value={{
+        email,
+        setEmail,
+        isLogged,
+        setIsLogged
+      }}
+    >
+      {children}
+    </FakeLoginContext.Provider>
+  );
+};
+
+export default FakeLoginContext;
