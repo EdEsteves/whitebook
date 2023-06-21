24f1431daac0745123072d34e814ce1390979677
Eduardo <eduardoestentotecinfo@gmail.com>
2023-06-20 21:30:36 -0300

    Create button component

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import * as S from './styles'
+
+interface ButtonProps {
+  text: string;
+  onClick: () => void;
+}
+
+const Button: React.FC<ButtonProps> = ({ text, onClick }) => {
+  return (
+    <S.Button onClick={onClick}>
+      {text}
+    </S.Button>
+  );
+};
+
+export default Button;
---
+++ src/components/Button/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components'
+
+export const Button = styled.button`
+    font-weight: 700;
+    font-size: 12px;
+    line-height: 15px;
+    color: #fff;
+    background: #191847;
+    border-radius: 25px;
+    padding: 18px;
+    text-transform: uppercase;
+    border: none;
+    cursor: pointer;
+`
